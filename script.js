e8e8d5e6b6359764bdda05ceca7fbd20c3e10006
YKAwarta <150530332+YKAwarta@users.noreply.github.com>
2024-06-09 00:14:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("speakBtn").addEventListener("click", async function() {
     const text = document.getElementById("textInput").value;
     const apiKey = "72dcc43a4793b85e294f28c75475caef";
-    const voiceId = "A9ATTqUUQ6GHu0coCz8t";
+    const voiceId = "mRdG9GYEjJmIzqbYTidv";
     const url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`;
 
     const response = await fetch(url, {
